c5dfec8c5bfd14ec70ac7595216f4cf22cdcfb3d
20161104589 <a20161104589@admindeiMac-2.local>
2018-09-12 11:25:23 +0800

    一到一百的和
    一到一百的和

---
+++ swift1/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  swift1
+//
+//  Created by 20161104589 on 18/9/12.
+//  Copyright © 2018年 20161104589. All rights reserved.
+//
+
+import Foundation
+
+var s=0
+for i in 1...100{
+    s = s + i
+    print(s)
+}
